350128c00654eb93b0d46cfd341c9bfdfdd3544b
Deniz Gökbaş <dng.develops@gmail.com>
2022-08-11 16:39:40 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+//Selectors
+const navItems = document.querySelector("#nav__items");
+const openNavBtn = document.querySelector("#open__nav-btn");
+const closeNavBtn = document.querySelector("#close__nav-btn");
+
+//Event Listeners
+openNavBtn.addEventListener("click", () => {
+  navItems.style.display = "flex";
+  openNavBtn.style.display = "none";
+  closeNavBtn.style.display = "inline-block";
+});
+
+// GLobal Functions
+const closeNav = () => {
+  navItems.style.display = "none";
+  openNavBtn.style.display = "inline-block";
+  closeNavBtn.style.display = "none";
+};
+
+closeNavBtn.addEventListener("click", closeNav);
+
+console.log(navItems);

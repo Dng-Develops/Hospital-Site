401854c6b1fe4a7dd07af466c1357656ea453426
Deniz Gökbaş <dng.develops@gmail.com>
2022-09-06 13:24:49 +0300

    Bugfix in the navbar

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ const navItems = document.querySelector("#nav__items");
 const openNavBtn = document.querySelector("#open__nav-btn");
 const closeNavBtn = document.querySelector("#close__nav-btn");
 
-//Event Listeners
-openNavBtn.addEventListener("click", () => {
-  navItems.style.display = "flex";
-  openNavBtn.style.display = "none";
-  closeNavBtn.style.display = "inline-block";
-});
-
 // GLobal Functions
 const closeNav = () => {
   navItems.style.display = "none";
   openNavBtn.style.display = "inline-block";
   closeNavBtn.style.display = "none";
 };
+
+//Event Listeners
+openNavBtn.addEventListener("click", () => {
+  navItems.style.display = "flex";
+  openNavBtn.style.display = "none";
+  closeNavBtn.style.display = "inline-block";
+});
 
 closeNavBtn.addEventListener("click", closeNav);
 
